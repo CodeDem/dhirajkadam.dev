a47cf1d6581016d590dffd2be6a6c5b61c6a67e2
Dhiraj <kadamdhiraj96@gmail.com>
2020-04-19 16:10:16 +0530

    feat(config): ✨Add google analytics support

--- gatsby-config.js
+++ gatsby-config.js
@@ -241,5 +241,36 @@ module.exports = {
         shortname: `dhirajkadam-dev`
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        // optimizeId: process.env.GOOGLE_OPTIMIZE_TRACKING_ID,
+
+        // Enables Google Optimize Experiment ID
+        // experimentId: process.env.GOOGLE_EXPERIMENT_ID,
+
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: process.env.GOOGLE_OPTIMIZE_VARIATION_ID,
+
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
   ],
 }
